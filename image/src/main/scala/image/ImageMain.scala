9acce4157778a72117889ae6098b28e1f79afc4c
Olzhas <turlybekov.o@phystech.edu>
2023-10-01 23:02:55 +0300

    Added image service

---
+++ image/src/main/scala/image/ImageMain.scala
@@ -0,0 +1,19 @@
+package image
+
+import image.api.HttpRoutes
+import image.config.ServiceConfig
+import zio.http.Server
+import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
+
+object ImageMain extends ZIOAppDefault {
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
+    for {
+      _ <- ZIO.logInfo("Start ImageMain")
+      _ <- zio.http.Server.serve(HttpRoutes.app)
+        .provide(
+          Server.live,
+          ServiceConfig.live,
+        )
+    } yield ()
+  }
+}
---
+++ image/src/main/scala/image/api/HttpRoutes.scala
@@ -0,0 +1,30 @@
+package image.api
+
+import zio.ZIO
+import zio.http._
+import zio.http.model.{Method, Status}
+import zio.stream.{ZPipeline, ZSink}
+
+import java.nio.file.{Files, Paths}
+
+object HttpRoutes {
+  val app: HttpApp[Any, Response] =
+    Http.collectZIO[Request] {
+      case req@Method.PUT -> !! / "upload" / id =>
+        (for {
+          path <- ZIO.attempt(
+            Files.createFile(
+              Paths.get("images/" + id)
+            )
+          )
+          bytesCount <- req.body.asStream
+            .via(ZPipeline.deflate())
+            .run(ZSink.fromPath(path))
+        } yield bytesCount).either.map {
+          case Right(bytesCount) if bytesCount <= 10 * 1024 * 1024 => Response.ok
+          case _ =>
+            Files.deleteIfExists(Paths.get("images/" + id))
+            Response.status(Status.BadRequest)
+        }
+    }
+}
