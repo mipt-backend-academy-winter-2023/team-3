bfc9313f6cdf116bfd74b0f5cede5ea79d1c9d20
Olzhas <turlybekov.o@phystech.edu>
2023-10-20 00:08:48 +0300

    Code style fixed

--- auth/src/main/scala/auth/api/HttpRoutes.scala
+++ auth/src/main/scala/auth/api/HttpRoutes.scala
@@ -12,7 +12,7 @@ import zio.http.model.{Method, Status}
 object HttpRoutes {
   val app: HttpApp[UsersRepository, Response] =
     Http.collectZIO[Request] {
-      case req@Method.POST -> !! / "auth" / "v1" / "login" =>
+      case req @ Method.POST -> !! / "auth" / "v1" / "login" =>
         (for {
           bodyStr <- req.body.asString
           user <- ZIO.fromEither(decode[User](bodyStr)).tapError(e => ZIO.logError(e.getMessage))
@@ -20,7 +20,7 @@ object HttpRoutes {
         } yield found).either.map {
           case Right(user) =>
             user match {
-              case Some(_) => Response.json(s"{\"token\": \"${JwtService.encode(user.get.login)}\"}")
+              case Some(_) => Response.json(String.format("{\"token\": \"%s\"}", JwtService.encode(user.get.login)))
               case None =>
                 ZIO.logInfo(s"User '${user.get.login}' not found")
                 Response.status(Status.Unauthorized)
@@ -28,16 +28,17 @@ object HttpRoutes {
           case Left(_) => Response.status(Status.BadRequest)
         }
 
-      case req@Method.POST -> !! / "auth" / "v1" / "register" =>
+      case req @ Method.POST -> !! / "auth" / "v1" / "register" =>
         (for {
           bodyStr <- req.body.asString
           user <- ZIO.fromEither(decode[User](bodyStr)).tapError(e => ZIO.logError(e.getMessage))
-          _ <- UsersRepository.add(user)
+          _ <- UsersRepository
+            .add(user)
             .tapError(e => ZIO.logInfo(s"User '${user.login}' already exist(${e.getMessage})"))
           _ <- ZIO.logInfo(s"Created new user '${user.login}'")
         } yield ()).either.map {
           case Right(_) => Response.status(Status.Created)
-          case Left(_) => Response.status(Status.BadRequest)
+          case Left(_)  => Response.status(Status.BadRequest)
         }
     }
 }
