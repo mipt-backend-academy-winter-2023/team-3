29b16341d94dd7ab6efa5023d82ee33ca9218097
Olzhas <turlybekov.o@phystech.edu>
2023-10-25 22:50:09 +0300

    Fix

--- image/src/main/scala/image/api/HttpRoutes.scala
+++ image/src/main/scala/image/api/HttpRoutes.scala
@@ -5,38 +5,42 @@ import zio.http._
 import zio.http.model.{Method, Status}
 import zio.stream.ZSink
 
-import java.io.File
-import java.nio.file.{Files, Paths}
+import java.nio.file.{Files, Paths, Path}
 
 object HttpRoutes {
+  private val MaxFileSize = 10 * 1024 * 1024
+
   val app: HttpApp[Any, Response] =
     Http.collectZIO[Request] {
-      case req@Method.PUT -> !! / "upload" / id =>
+      case req @ Method.PUT -> !! / "upload" / id =>
+        val path = getPath(id)
+        ensureDirectoryExists(path.getParent)
         (for {
-          path <- ZIO.attempt(
-            Files.createFile(
-              Paths.get("./images/" + id + ".jpeg")
-            )
-          )
+          _ <- ZIO.attempt(Files.createFile(path))
           bytesCount <- req.body.asStream
             .run(ZSink.fromPath(path))
         } yield bytesCount).either.map {
-          case Right(bytesCount) if bytesCount <= 10 * 1024 * 1024 => Response.ok
+          case Right(bytesCount) if bytesCount <= MaxFileSize => Response.ok
           case _ =>
-            Files.deleteIfExists(Paths.get("images/" + id))
+            Files.deleteIfExists(path)
             Response.status(Status.UnprocessableEntity)
         }
-      case req@Method.GET -> !! / "download" / id =>
-        val imagePath = Paths.get("./images/" + id + ".jpeg")
-        if (Files.exists(imagePath)) {
-          ZIO.succeed(
-            Response(
-              status = Status.Ok,
-              body = Body.fromFile(new File(imagePath.toAbsolutePath.toString))
-            )
-          )
+      case _ @Method.GET -> !! / "download" / id =>
+        val path = getPath(id)
+        if (Files.exists(path)) {
+          ZIO.succeed(Response(status = Status.Ok, body = Body.fromFile(path.toFile)))
         } else {
           ZIO.succeed(Response.status(Status.NotFound))
         }
     }
+
+  private def getPath(id: String): Path = {
+    Paths.get(s"./images/$id.jpeg")
+  }
+
+  private def ensureDirectoryExists(path: Path): Unit = {
+    if (!Files.exists(path)) {
+      Files.createDirectories(path)
+    }
+  }
 }
