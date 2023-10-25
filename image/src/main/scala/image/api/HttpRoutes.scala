dd0153a5dd51edd9aeeac89b29cadedd1907f765
OwOlzhas <turkybekovolzhas@mail.ru>
2023-10-25 23:47:10 +0300

    Fix

--- image/src/main/scala/image/api/HttpRoutes.scala
+++ image/src/main/scala/image/api/HttpRoutes.scala
@@ -6,7 +6,9 @@ import zio.http._
 import zio.http.model.{Method, Status}
 import zio.stream.ZSink
 
-import java.nio.file.{Files, Paths, Path}
+import java.nio.file.{FileAlreadyExistsException, Files, Path, Paths}
+
+case object FileAlreadyExistError
 
 object HttpRoutes {
   private val MaxFileSize = 10 * 1024 * 1024
@@ -17,12 +19,14 @@ object HttpRoutes {
         val path = getPath(id)
         ensureDirectoryExists(path.getParent)
         (for {
-          _ <- ZIO.attempt(Files.createFile(path))
+          _ <- ZIO.attempt(Files.createFile(path)).mapError(_ => FileAlreadyExistError)
           bytesCount <- req.body.asStream
             .via(JpegValidation.pipeline)
             .run(ZSink.fromPath(path))
         } yield bytesCount).either.map {
           case Right(bytesCount) if bytesCount <= MaxFileSize => Response.ok
+          case Left(FileAlreadyExistError) =>
+            Response.status(Status.BadRequest)
           case _ =>
             Files.deleteIfExists(path)
             Response.status(Status.UnprocessableEntity)
