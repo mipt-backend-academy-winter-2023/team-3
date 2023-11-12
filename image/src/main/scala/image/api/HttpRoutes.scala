f085b072e4286e0b236482221816691fea996eae
Олжас Турлыбеков <79211346+OvOlzhas@users.noreply.github.com>
2023-11-12 21:06:51 +0300

    Image service added (#17)

---
+++ image/src/main/scala/image/api/HttpRoutes.scala
@@ -0,0 +1,59 @@
+package image.api
+
+import zio.ZIO
+import zio.http._
+import zio.http.model.{Header, Headers, Method, Status}
+import zio.stream.{ZSink, ZStream}
+
+import java.nio.file.{Files, Path, Paths}
+
+case object FileAlreadyExistError
+
+object HttpRoutes {
+  private val MaxFileSize = 10 * 1024 * 1024
+
+  val app: HttpApp[Any, Response] =
+    Http.collectZIO[Request] {
+      case req @ Method.PUT -> !! / "upload" / id =>
+        val path = getPath(id)
+        ensureDirectoryExists(path.getParent)
+        (for {
+          _ <- ZIO.attempt(Files.createFile(path)).mapError(_ => FileAlreadyExistError)
+          bytesCount <- req.body.asStream
+            .run(ZSink.fromPath(path))
+        } yield bytesCount).either.map {
+          case Right(bytesCount) if bytesCount <= MaxFileSize =>
+            Response(
+              status = Status.Ok,
+              headers = Headers(
+                Header("Content-Length", Files.size(path).toString)
+              ),
+              body = Body.fromString("Success")
+            )
+          case Left(FileAlreadyExistError) =>
+            Response.apply(status = Status.BadRequest, body = Body.fromString("File is already exist"))
+          case Left(err) =>
+            Response.apply(status = Status.BadRequest, body = Body.fromString(err.toString))
+          case _ =>
+            Files.deleteIfExists(path)
+            Response.apply(status = Status.UnprocessableEntity, body = Body.fromString("The file size exceeds 10 MB"))
+        }
+      case _ @Method.GET -> !! / "download" / id =>
+        val path = getPath(id)
+        if (Files.exists(path)) {
+          ZIO.succeed(Response(status = Status.Ok, body = Body.fromStream(ZStream.fromFile(path.toFile))))
+        } else {
+          ZIO.succeed(Response.status(Status.NotFound))
+        }
+    }
+
+  private def getPath(id: String): Path = {
+    Paths.get(s"./images/$id.jpeg")
+  }
+
+  private def ensureDirectoryExists(path: Path): Unit = {
+    if (!Files.exists(path)) {
+      Files.createDirectories(path)
+    }
+  }
+}
---
+++ image/src/main/scala/image/config/Config.scala
@@ -0,0 +1,20 @@
+package image.config
+
+import pureconfig.ConfigSource
+import pureconfig.generic.auto.exportReader
+import zio.http.ServerConfig
+import zio.{ZLayer, http}
+
+case class Config(host: String, port: Int)
+
+object ServiceConfig {
+  private val source = ConfigSource.default.at("app").at("http-service")
+  private val serviceConfig: Config = source.loadOrThrow[Config]
+
+  val live: ZLayer[Any, Nothing, ServerConfig] = zio.http.ServerConfig.live {
+    http
+      .ServerConfig
+      .default
+      .binding(serviceConfig.host, serviceConfig.port)
+  }
+}
