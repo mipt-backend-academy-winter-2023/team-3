58ae12835f86b20f35102f02c8856c6dfbc3e640
mukhametgalin <mukhametgalin.ad@phystech.edu>
2023-11-19 22:45:22 +0300

    fixed merge issues

--- src/main/scala/StartApp.scala
+++ src/main/scala/StartApp.scala
@@ -7,14 +7,9 @@ import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 object StartApp extends ZIOAppDefault {
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
     for {
-<<<<<<< HEAD
       _ <- RoutingMain.run
       _ <- AuthMain.run
       _ <- ImageMain.run
-=======
-      // _ <- RoutingMain.run
-      // _ <- AuthMain.run
->>>>>>> ea05714 (formatted files)
       _ <- HelperMain.run
     } yield ()
 }
